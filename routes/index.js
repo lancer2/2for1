039d712c62e07b46614fd5921b69ae5965560daa
Lancer <88027257@qq.com>
2015-09-11 17:23:56 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,33 @@
-module.exports = function(app){
+module.exports = function (app) {
 
 
-  app.get('/', function(req, res, next) {
-    if(req.session.user){
-      res.render('index',{'user':req.session.user});
-    }else{
-      req.session.error = "Error:请先登录!";
-      res.render('login',{'error':req.session.error});
-    }
-  });
-  app.get('/:hbs',function(req,res){ if(req.session.user){
-      res.render(req.params.hbs,{'user':req.session.user});
-    }else{
-      req.session.error = "Error:请先登录!";
-      res.render('login',{'error':req.session.error});
-    }
-  });
+    app.get('/', function (req, res, next) {
+        if (req.session.user) {
+            res.render('index', {'user': req.session.user});
+        } else {
+            res.render('login', {'error': "Error:请先登录!"});
+        }
+    });
+    app.get('/:hbs', function (req, res) {
+        if (req.session.user) {
+            res.render(req.params.hbs, {'user': req.session.user});
+        } else {
+            res.render('login', {'error': "Error:请先登录!"});
+        }
+    });
 
-  app.post('/login', function(req, res) {
-    var user={
-      username:'admin',
-      password:'admin'
-    }
-    if(req.body.username==user.username&&req.body.password==user.password){
-      req.session.user = user;
-      //取消layout
-      console.log('=======post:'+user.username);
-      res.send(200);
-    }else{
-      req.session.error = "用户名或密码不正确";
-      res.send( 404 );
-    }
-  });
+    app.post('/login', function (req, res) {
+        var user = {
+            username: 'admin',
+            password: 'admin'
+        }
+        if (req.body.username == user.username && req.body.password == user.password) {
+            req.session.user = user;
+            //取消layout
+            res.send(200);
+        } else {
+            res.json({ "error": "用户名或密码错误" });
+            res.send(200);
+        }
+    });
 };
